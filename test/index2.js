dedd7927baa4c44b8adbef0ebfd6d77c04064e69
wangrui10 <wangrui10@baidu.com>
2015-05-26 14:32:20 +0800

    update test

--- test/index2.js
+++ test/index2.js
@@ -2,10 +2,10 @@
  * Created by ryan on 15/5/13.
  */
 var fs = require('fs'),
-  path   = require('path');
-var fis = require('fis');
+    path   = require('path');
+var fis = require('../../..');
 var _      = fis.util,
-  config = fis.config;
+    config = fis.config;
 var expect = require('chai').expect;
 var amd = require('../lib/amd.js');
 var commonJs = require('../lib/commonJs.js');
@@ -31,22 +31,23 @@ describe('index:', function () {
         content: file.getContent()
       };
       fis.emit('standard:js',info);
-      var str = "define('test3.js', function(require, exports, module) {\n" +
-        "var toString = Object.prototype.toString;var _ = module.exports = {};_.mode = {CAPTURE: 1,};" +
-        "\n\n" +
-        "});";
+      var str = "define('test3', function(require, exports, module) {\n" +
+          "var toString = Object.prototype.toString;var _ = module.exports = {};_.mode = {CAPTURE: 1,};" +
+          "\n\n" +
+          "});";
+
       expect(info.content.toString()==str).to.be.true;
 
       var filepath = path.join(root, 'test2.js');
       settings.type = "";
       settings.type = "auto";
       processor(fis, settings);
-        var file = fis.file.wrap(filepath);
-        var info = {
-          file: file,
-          content: file.getContent()
-        };
-        fis.emit('standard:js',info);
+      var file = fis.file.wrap(filepath);
+      var info = {
+        file: file,
+        content: file.getContent()
+      };
+      fis.emit('standard:js',info);
       var xstr = info.content.toString();
       expect(xstr.indexOf("jsRequire") == 19).to.be.true;
       expect((xstr.indexOf("jsRequire")-9) == xstr.indexOf("require")).to.be.true;
@@ -63,19 +64,20 @@ describe('index:', function () {
     project.setProjectRoot(root);
     var filepath = path.join(root, 'test.js');
     fis.util.write(filepath, 'console.log("hello, world.");');
+
     var info = project.lookup('/test.js');
     expect(info.id == "test.js").to.be.true;
-    expect(info.moduleId == "test.js").to.be.true;
+    expect(info.moduleId == "test").to.be.true;
 
     var info2 = project.lookup('/test');
     expect(info.id == "test.js").to.be.true;
-    expect(info.moduleId == "test.js").to.be.true;
+    expect(info.moduleId == "test").to.be.true;
 
     fis.config.env().set('namespaceConnector', '/');
     fis.config.env().set('namespace','project')
     var info2 = project.lookup('project/test');
     expect(info.id == "test.js").to.be.true;
-    expect(info.moduleId == "test.js").to.be.true;
+    expect(info.moduleId == "test").to.be.true;
   });
 
 });
